8a5537552693585da6a5ecce75b0a1f6f1179afd
crs2here <crs2here@gmail.com>
2019-12-17 22:22:23 -0800

    setup constants for easy config

---
+++ src/lib/mapConstants.js
@@ -0,0 +1,12 @@
+const LOCATION = {
+  lat: 31.51073,
+  lng: -96.4247
+};
+export const CENTER = [LOCATION.lat, LOCATION.lng];
+export const DEFAULT_ZOOM = 3;
+export const mapColors = {
+  congressional: { color: "blue" },
+  states: { color: "green" },
+  counties: { color: "orange" },
+  outline: { color: "red" }
+};
