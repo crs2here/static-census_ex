7ace4f568cfe6ebf629f7f7da6894ea9f69df99c
crs2here <crs2here@gmail.com>
2019-12-17 22:36:18 -0800

    clean up gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,8 @@
 module.exports = {
   plugins: [
-    'gatsby-plugin-resolve-src',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`
-      }
-    },
-    'gatsby-plugin-react-leaflet'
+    "gatsby-plugin-resolve-src",
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-react-leaflet"
   ]
 };
