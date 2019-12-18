6833dd3a5f972b2190cbe66609903a11b63d1316
crs2here <crs2here@gmail.com>
2019-12-17 22:20:00 -0800

    customize header and footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,12 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from "react";
 
-import Container from 'components/Container';
+import Container from "components/Container";
 
 const Header = () => {
   return (
     <header>
       <Container type="content">
-        <p>My Gatsby Site</p>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2/">Page 2</Link>
-          </li>
-        </ul>
+        <p>geojson and kml data for the united states</p>
       </Container>
     </header>
   );
