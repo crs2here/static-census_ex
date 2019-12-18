82dc00d63a44e918a067840f917166ebb1bf9286
crs2here <crs2here@gmail.com>
2019-12-17 22:22:54 -0800

    implement hooks to trigger map render

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,103 +1,78 @@
-import React, { useRef } from 'react';
-import Helmet from 'react-helmet';
-import L from 'leaflet';
-import { Marker } from 'react-leaflet';
+import React, { useState } from "react";
+import Helmet from "react-helmet";
+import { GeoJSON } from "react-leaflet";
 
-import { promiseToFlyTo, getCurrentLocation } from 'lib/map';
+import Layout from "components/Layout";
+import Container from "components/Container";
+import Map from "components/Map";
 
-import Layout from 'components/Layout';
-import Container from 'components/Container';
-import Map from 'components/Map';
-
-import gatsby_astronaut from 'assets/images/gatsby-astronaut.jpg';
-
-const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369
-};
-const CENTER = [LOCATION.lat, LOCATION.lng];
-const DEFAULT_ZOOM = 2;
-const ZOOM = 10;
-
-const timeToZoom = 2000;
-const timeToOpenPopupAfterZoom = 4000;
-const timeToUpdatePopupAfterZoom = timeToOpenPopupAfterZoom + 3000;
-
-const popupContentHello = `<p>Hello 👋</p>`;
-const popupContentGatsby = `
-  <div class="popup-gatsby">
-    <div class="popup-gatsby-image">
-      <img class="gatsby-astronaut" src=${gatsby_astronaut} />
-    </div>
-    <div class="popup-gatsby-content">
-      <h1>Gatsby Leaflet Starter</h1>
-      <p>Welcome to your new Gatsby site. Now go build something great!</p>
-    </div>
-  </div>
-`;
+import { congressional } from "../data/congressional.js";
+import { counties } from "../data/counties.js";
+import { outline } from "../data/outline.js";
+import { states } from "../data/states.js";
+import { CENTER, DEFAULT_ZOOM, mapColors } from "../lib/mapConstants";
 
 const IndexPage = () => {
-  const markerRef = useRef();
-
-  /**
-   * mapEffect
-   * @description Fires a callback once the page renders
-   * @example Here this is and example of being used to zoom in and set a popup on load
-   */
-
-  async function mapEffect({ leafletElement } = {}) {
-    if ( !leafletElement ) return;
-
-    const popup = L.popup({
-      maxWidth: 800
-    });
-
-    const location = await getCurrentLocation().catch(() => LOCATION );
-
-    const { current = {} } = markerRef || {};
-    const { leafletElement: marker } = current;
-
-    marker.setLatLng( location );
-    popup.setLatLng( location );
-    popup.setContent( popupContentHello );
-
-    setTimeout( async () => {
-      await promiseToFlyTo( leafletElement, {
-        zoom: ZOOM,
-        center: location
-      });
-
-      marker.bindPopup( popup );
-
-      setTimeout(() => marker.openPopup(), timeToOpenPopupAfterZoom );
-      setTimeout(() => marker.setPopupContent( popupContentGatsby ), timeToUpdatePopupAfterZoom );
-    }, timeToZoom );
-  }
+  const [geoJSONData, setgeoJSONData] = useState({
+    congressional: false,
+    states: false,
+    counties: false,
+    outline: false
+  });
 
   const mapSettings = {
     center: CENTER,
-    defaultBaseMap: 'OpenStreetMap',
-    zoom: DEFAULT_ZOOM,
-    mapEffect
+    defaultBaseMap: "OpenStreetMap",
+    zoom: DEFAULT_ZOOM
   };
+
+  const handleChange = event => {
+    const newState = { [event.target.value]: event.target.checked };
+
+    setgeoJSONData(prevState => ({ ...prevState, ...newState }));
+  };
+
+  const CBInput = ({ checked, name }) => (
+    <label style={mapColors[name]}>
+      {name}
+      <input
+        style={{ width: "15%" }}
+        checked={checked}
+        name={name}
+        value={name}
+        type="checkbox"
+        id={name}
+        onChange={handleChange}
+      />
+    </label>
+  );
 
   return (
     <Layout pageName="home">
       <Helmet>
-        <title>Home Page</title>
+        <title>geojson</title>
       </Helmet>
 
       <Map {...mapSettings}>
-        <Marker ref={markerRef} position={CENTER} />
+        {geoJSONData.congressional && (
+          <GeoJSON style={mapColors.congressional} data={congressional.data} />
+        )}
+        {geoJSONData.states && (
+          <GeoJSON style={mapColors.states} data={states.data} />
+        )}
+        {geoJSONData.counties && (
+          <GeoJSON style={mapColors.counties} data={counties.data} />
+        )}
+        {geoJSONData.outline && (
+          <GeoJSON style={mapColors.outline} data={outline.data} />
+        )}
       </Map>
 
-      <Container type="content" className="text-center home-start">
-        <h2>Still Getting Started?</h2>
-        <p>Run the following in your terminal!</p>
-        <pre>
-          <code>gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-leaflet</code>
-        </pre>
-        <p className="note">Note: Gatsby CLI required globally for the above command</p>
+      <Container type="content" className="home-start">
+        <CBInput checked={geoJSONData.congressional} name={"congressional"} />
+        <CBInput checked={geoJSONData.states} name={"states"} />
+        <CBInput checked={geoJSONData.counties} name={"counties"} />
+        <CBInput checked={geoJSONData.outline} name={"outline"} />
       </Container>
     </Layout>
   );
